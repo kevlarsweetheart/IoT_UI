465bba94c45d80f0f9b86fb14fe133e808c28e44
Maria Efremova <maria@dell7537-masha>
2017-04-14 20:54:45 +0300

    main page with date and time

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-function time_date(id) {
-  var today = new Date();
-  var dd = today.getDate();
-  var mm = today.getMonth()+1; //January is 0!
-  var yyyy = today.getFullYear();
+var MOSCOW_OFF = 3; // hours
+var MONTHS = ["January", "February", "March", "April", "May", "June", "July",
+              "August", "September", "October", "November", "December"];
 
-  if(dd<10) {
-      dd='0'+dd
-  }
-
-  if(mm<10) {
-      mm='0'+mm
-  }
-
-  today = dd+'.'+mm+'.'+yyyy;
-  document.getElementById("time-date").innerHTML = today;
-  return true;
+// desired format => November 1, 2017 21:31:00
+function getLocalTime(offset) {
+    var d = new Date();
+    d.setTime((new Date().getTime()) +
+              (d.getTimezoneOffset() * 60 * 1000) + // local offset
+              (1000 * 60 * 60 * offset)); // target offset
+    var day = MONTHS[d.getMonth()] + " " + d.getDate() + ", " + d.getFullYear();
+    var time = d.toTimeString().split(" ")[0];
+    var time_date = time + " / " + day;
+    document.getElementById("time-date").innerHTML = time_date;
+    setTimeout(getLocalTime(MOSCOW_OFF), 1000);
 }
+
+getLocalTime(MOSCOW_OFF);
